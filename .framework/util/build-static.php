cc2874678a2a7082de07246082ec07fdb4169089
polarnix <polarnix@pm.me>
2023-02-18 00:47:04 -0700

    cleaned up file

--- .framework/util/build-static.php
+++ .framework/util/build-static.php
@@ -1,7 +1,6 @@
 <?php
 
 $site_port = '3000';
-
 $site_content = file_get_contents('http://localhost:' . $site_port . '');
 
 $tidy_config = array(
@@ -9,16 +8,12 @@ $tidy_config = array(
   'output-xhtml' => true,
   'wrap' => 200
 );
-
 $tidy = new \tidy;
 $tidy->parseString($site_content, $tidy_config, 'utf8');
 $tidy->cleanRepair();
 
 $clean_site_content = (string) $tidy;
-
 $js_path = '.framework/build/script.js';
-
 $fixed_site_content = str_replace($js_path, '../build/script.js', $clean_site_content);
-
 
 file_put_contents('../build-static/index.html', $fixed_site_content);
